4e69912a9a6ca76237e71463062b7d9178285593
keisuna <zaq12wsx594@yahoo.co.jp>
2017-10-06 13:32:22 +0900

    beta0.26

--- ls26/myWebVR.js
+++ ls26/myWebVR.js
@@ -33,20 +33,14 @@ function webvr_click() {
         fullScreen();
         gameInstance.SendMessage('CameraSet', 'ChangeMode', 'vr');
         mode = 1;
-        var button = document.getElementById('b');
-        button.disabled = false;
         var button = document.getElementById('button');
-        button.disabled = true;
+        button.value = "Stop VR";
     } else
     {
         console.log("Display Normal");
         vrHMD.exitPresent();
         gameInstance.SendMessage('CameraSet', 'ChangeMode', 'normal');
         mode = 0;
-        var button = document.getElementById('button');
-        button.disabled = false;
-        var button = document.getElementById('b');
-        button.disabled = true;
     }
     //getVRSensorState();
 }
